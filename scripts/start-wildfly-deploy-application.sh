368c35b4caf15ddad27bc069495e8841ecce2c1a
BenGalewsky <ben@peartreestudio.net>
2017-07-10 19:17:19 -0500

    Adding seed sql

--- scripts/start-wildfly-deploy-application.sh
+++ scripts/start-wildfly-deploy-application.sh
@@ -21,6 +21,10 @@ function wait_for_wildfly {
 }
 
 createdb -U postgres psm
+psql -U postgres -w -f ${TRAVIS_BUILD_DIR}/psm-app/db/legacy_seed.sql
+psql -U postgres -w -f ${TRAVIS_BUILD_DIR}/psm-app/db/seed.sql
+psql -U postgres -w -f ${TRAVIS_BUILD_DIR}/psm-app/db/jbpm.sql
+
 
 ${WILDFLY_HOME}/bin/standalone.sh -c standalone-full.xml > ${WILDFLY_LOG} &
 
